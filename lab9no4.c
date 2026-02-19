706b2ccf0503601f0200d2980a85ae56be51989e
6704062610119 <s6704062610119@email.kmutnb.ac.th>
2026-02-20 01:58:47 +0700

    commit2
    swap array1d

--- lab9no4.c
+++ lab9no4.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<stdio.h> //finish commit2 but not sure if it's right
 
 void swapValue(int *a, int *b, int *c);
 void swapArray(int *a, int *b, int size);
@@ -9,9 +9,31 @@ int main(){
 	printf("Before swap function: a=%d, b=%d, c=%d\n", a, b, c);
 	swapValue( &a,&b,&c );
 	printf("After swap function: a= %d, b=%d, c=%d\n", a, b, c);
+	printf("\n");
 	
-	//calling swapArray()
-
+	int arr[5]={1,2,3,4,5}, brr[5]={6,7,8,9,10}, i;
+	printf("Before swap array:\n");
+	printf("arr = ");
+	for(i=0;i<5;i++){
+		printf("%d ",arr[i]);
+	}
+	printf("\n");
+	printf("brr = ");
+	for(i=0;i<5;i++){
+		printf("%d ",brr[i]);
+	}
+	printf("\n");
+	swapArray(arr,brr,5);
+	printf("After swap array:\n");
+	printf("arr = ");
+	for(i=0;i<5;i++){
+		printf("%d ",arr[i]); 
+	}
+	printf("\n");
+	printf("brr = ");
+	for(i=0;i<5;i++){
+		printf("%d ",brr[i]);
+	}
 	
 	return 0;
 }
@@ -22,4 +44,15 @@ void swapValue(int *a, int *b, int *c){
 	*b = *c;
 	*c = temp;
 };
+
+void swapArray(int *a, int *b, int size){
+    int i, temp;
+    for (i = 0; i < size; i++) {
+        temp = a[i];
+        a[i] = b[i];
+        b[i] = temp;
+	}
+}; 
+
+
 
